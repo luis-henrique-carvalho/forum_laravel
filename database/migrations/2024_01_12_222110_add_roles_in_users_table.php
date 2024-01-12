352f9c4de9e004d243ace41f2615dbe9ceda5400
luicarvalho <luis.carvalho@mpmt.mp.br>
2024-01-12 20:45:50 -0300

    :recycle: refactor: RolesEnum in users table migration

--- database/migrations/2024_01_12_222110_add_roles_in_users_table.php
+++ database/migrations/2024_01_12_222110_add_roles_in_users_table.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Enums\RolesEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Enums\RolesEnum;
 
 return new class extends Migration
 {
