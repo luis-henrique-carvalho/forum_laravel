2b85771b0ee9a6601e6cc3ba901eebdc1f697ac7
luicarvalho <luis.carvalho@mpmt.mp.br>
2024-01-12 21:56:29 -0300

    :sparkles: feat: Add Portuguese translations for menu and adminlte

---
+++ resources/lang/pt-br/menu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'main_navigation' => 'Navegação Principal',
+    'blog' => 'Blog',
+    'pages' => 'Páginas',
+    'supports' => 'Suporte',
+    'profile' => 'Perfil',
+    'change_password' => 'Mudar Senha',
+    'multilevel' => 'Multinível',
+    'level_one' => 'Nível 1',
+    'level_two' => 'Nível 2',
+    'level_three' => 'Nível 3',
+    'labels' => 'Etiquetas',
+    'Important' => 'Importante',
+    'Warning' => 'Aviso',
+    'Information' => 'Informação',
+];
