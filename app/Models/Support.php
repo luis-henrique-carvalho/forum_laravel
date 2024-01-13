65320a9f584b6c7e53cb03a05e101188556c6580
luicarvalho <luis.carvalho@mpmt.mp.br>
2024-01-12 23:32:37 -0300

    :sparkles: feat: Add fillable and saveRules to Support model

--- app/Models/Support.php
+++ app/Models/Support.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Support extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'subject',
+        'body',
+    ];
+
+    public static function saveRules(): array
+    {
+        return [
+            'subject' => 'required|string|max:255',
+            'body' => 'required|string',
+        ];
+    }
 }
